e884e2dd7b9118c48190ad93919400b833af09d7
boozy <boozy@example.com>
2016-02-28 00:07:59 +0200

    Testing new text editor

--- License.php
+++ License.php
@@ -6,3 +6,7 @@ Just Bla and nothing more!!!
 Just Bla and nothing more!!!
 Just Bla and nothing more!!!
 Just Bla and nothing more!!!
+
+
+C:\Program Files (x86)\Notepad++
+C:\Program Files (x86)\Notepad++
